5f551459bfe86f78828f12005736f6c9202876b4
Flydexo <flydexo@gmail.com>
2023-03-10 20:14:40 +0100

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://permissive.dev',
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+  output: "server",
+  adapter: vercel()
 });
